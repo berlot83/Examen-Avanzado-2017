e58b2609cb9f41dafeacde7265659361711a20e7
Axel Berlot <berlot83@yahoo.com.ar>
2017-11-22 09:51:28 -0300

    Update TestGetReseller.java

--- ExamenIAvanzado/src/com/utn/view/TestGetReseller.java
+++ ExamenIAvanzado/src/com/utn/view/TestGetReseller.java
@@ -5,7 +5,7 @@ import com.utn.dao.factory.ResellerrFactory;
 public class TestGetReseller {
 	public static void main(String[] args) {
 
-		/* Devuelve List<Reseller> */
+		/* Devuelve List<Reseller> y crea un txt en path/manaos/nombre.txt y en el proyecto local */
 		ResellerrFactory rs = new ResellerrFactory();
 		rs.getReseller();
 	}
